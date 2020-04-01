544b007930cb32edd6fa9accec140d95d505dd21
unknown <rizka1800016083@webmail.uad.ac.id>
2020-04-01 18:34:33 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Praktikum1Component } from './pages/praktikum1/praktikum1.component';
+import { Praktikum2Component } from './pages/praktikum2/praktikum2.component';
+import { Praktikum3Component } from './pages/praktikum3/praktikum3.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ 
+		path:'Praktikum1',
+		component:Praktikum1Component
+	},
+
+	{
+		path:'Praktikum2',
+		component:Praktikum2Component
+	},
+
+	{
+		path:'Praktikum3',
+		component:Praktikum3Component
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Praktikum1Component } from './pages/praktikum1/praktikum1.component';
+import { Praktikum2Component } from './pages/praktikum2/praktikum2.component';
+import { Praktikum3Component } from './pages/praktikum3/praktikum3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Praktikum1Component,
+    Praktikum2Component,
+    Praktikum3Component
   ],
   imports: [
     BrowserModule,
